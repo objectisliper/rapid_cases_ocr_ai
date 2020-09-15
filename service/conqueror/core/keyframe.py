de0245cab7d38199bc081b89a8d4bb84321d0df2
v.morozov <dude.ksu@gmail.com>
2020-09-15 17:11:56 +0300

    Revert method locations

--- service/conqueror/core/keyframe.py
+++ service/conqueror/core/keyframe.py
@@ -58,6 +58,47 @@ class KeyFrameFinder:
 
         self.__load_recognition_settings(recognition_settings)
 
+    def __load_recognition_settings(self, recognition_settings):
+        settings = recognition_settings.keys()
+        if "skip_frames" in settings: self.skip_frames = recognition_settings["skip_frames"]
+
+        if "use_gray_colors" in settings: self.use_gray_colors = recognition_settings["use_gray_colors"]
+
+        if "invert_colors" in settings: self.invert_colors = recognition_settings["invert_colors"]
+
+        if "use_morphology" in settings: self.use_morphology = recognition_settings["use_morphology"]
+
+        if "use_threshold_with_gausian_blur" in settings: self.use_threshold_with_gausian_blur = recognition_settings["use_threshold_with_gausian_blur"]
+
+        if "use_adaptiveThreshold" in settings: self.use_adaptiveThreshold = recognition_settings["use_adaptiveThreshold"]
+
+        if "comparing_similarity_for_phrases" in settings: self.comparing_similarity_for_phrases = recognition_settings["comparing_similarity_for_phrases"]
+
+    def __save_recognition_csv(self, recognition_data):
+        import datetime, os
+        time_suffix = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        report_filename = os.path.join("recognize_dict" + time_suffix + ".csv")
+        try:
+            with open(report_filename, 'w', encoding='utf-8', newline='') as csv_file:
+                writer = csv.writer(csv_file)
+                writer.writerow(list(recognition_data.keys()))
+                for index, text in enumerate(recognition_data['text']):
+                    row = [recognition_data['level'][index],
+                                     recognition_data['page_num'][index],
+                                     recognition_data['block_num'][index],
+                                     recognition_data['par_num'][index],
+                                     recognition_data['line_num'][index],
+                                     recognition_data['word_num'][index],
+                                     recognition_data['left'][index],
+                                     recognition_data['top'][index],
+                                     recognition_data['width'][index],
+                                     recognition_data['height'][index],
+                                     recognition_data['conf'][index],
+                                     text]
+                    writer.writerow(row)
+        except IOError:
+            print("I/O error")
+
     def process_keyframes(self) -> ([str], dict, dict):
         if not self.byte_video:
             return self.found_lines, self.url_contains_result, self.text_contains_result
@@ -206,81 +247,37 @@ class KeyFrameFinder:
 
         return image
 
-    def __load_recognition_settings(self, recognition_settings):
-        settings = recognition_settings.keys()
-        if "skip_frames" in settings: self.skip_frames = recognition_settings["skip_frames"]
-
-        if "use_gray_colors" in settings: self.use_gray_colors = recognition_settings["use_gray_colors"]
-
-        if "invert_colors" in settings: self.invert_colors = recognition_settings["invert_colors"]
-
-        if "use_morphology" in settings: self.use_morphology = recognition_settings["use_morphology"]
-
-        if "use_threshold_with_gausian_blur" in settings: self.use_threshold_with_gausian_blur = recognition_settings[
-            "use_threshold_with_gausian_blur"]
-
-        if "use_adaptiveThreshold" in settings: self.use_adaptiveThreshold = recognition_settings[
-            "use_adaptiveThreshold"]
-
-        if "comparing_similarity_for_phrases" in settings: self.comparing_similarity_for_phrases = recognition_settings[
-            "comparing_similarity_for_phrases"]
-
-    def __save_recognition_csv(self, recognition_data):
-        import datetime, os
-        time_suffix = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-        report_filename = os.path.join("recognize_dict" + time_suffix + ".csv")
-        try:
-            with open(report_filename, 'w', encoding='utf-8', newline='') as csv_file:
-                writer = csv.writer(csv_file)
-                writer.writerow(list(recognition_data.keys()))
-                for index, text in enumerate(recognition_data['text']):
-                    row = [recognition_data['level'][index],
-                           recognition_data['page_num'][index],
-                           recognition_data['block_num'][index],
-                           recognition_data['par_num'][index],
-                           recognition_data['line_num'][index],
-                           recognition_data['word_num'][index],
-                           recognition_data['left'][index],
-                           recognition_data['top'][index],
-                           recognition_data['width'][index],
-                           recognition_data['height'][index],
-                           recognition_data['conf'][index],
-                           text]
-                    writer.writerow(row)
-        except IOError:
-            print("I/O error")
+    def __check_text_contains(self, whole_page_text):
+        for key in self.text_contains_result.keys():
+            if not self.text_contains_result[key] \
+               and len(whole_page_text) + 5 >= len(key) \
+               and (
+                   key in whole_page_text
+                   or
+                   fuzz.partial_ratio(key, whole_page_text) >= self.comparing_similarity_for_phrases
+               ):
+                self.text_contains_result[key] = True
 
     def __check_url_contains(self, whole_page_text):
         for key in self.url_contains_result.keys():
             if not self.url_contains_result[key] \
-                    and len(whole_page_text) + 5 >= len(key) \
-                    and (
-                    key.lower() in whole_page_text.lower()
-                    or
-                    fuzz.partial_ratio(key, whole_page_text) >= self.comparing_similarity_for_phrases
-            ):
+               and len(whole_page_text) + 5 >= len(key) \
+               and (
+                   key.lower() in whole_page_text.lower()
+                   or
+                   fuzz.partial_ratio(key, whole_page_text) >= self.comparing_similarity_for_phrases
+               ):
                 self.url_contains_result[key] = True
-
-    def __check_text_contains(self, whole_page_text):
-        for key in self.text_contains_result.keys():
-            if not self.text_contains_result[key] \
-                    and len(whole_page_text) + 5 >= len(key) \
-                    and (
-                    key in whole_page_text
-                    or
-                    fuzz.partial_ratio(key, whole_page_text) >= self.comparing_similarity_for_phrases
-            ):
-                self.text_contains_result[key] = True
 
     def __save_if_keyphrase(self, text):
         for phrase in self.search_phrases:
             if text not in self.found_lines \
                     and len(text) + 5 >= len(phrase) \
                     and (
-                    fuzz.partial_ratio(phrase, text) >= self.comparing_similarity_for_phrases
-                    or
-                    phrase.lower() in text.lower()
-            ):
+                        fuzz.partial_ratio(phrase, text) >= self.comparing_similarity_for_phrases
+                        or
+                        phrase.lower() in text.lower()
+                    ):
                 self.found_lines.append(text)
 
     def __get_page_text_by_lines(self, image_data: dict):
